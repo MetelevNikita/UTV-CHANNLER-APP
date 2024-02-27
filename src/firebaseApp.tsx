3da6dfbd5386f30ded4a3f0d25c0bda40a51441a
MetelevNikita <Kyle.B@mail.ru>
2024-02-27 10:57:53 +0500

    first commit

---
+++ src/firebaseApp.tsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAmE5Z0UIp6k4qZvKhXayRJf-JHL4pIEEs",
+  authDomain: "utv-cnannel-app.firebaseapp.com",
+  projectId: "utv-cnannel-app",
+  storageBucket: "utv-cnannel-app.appspot.com",
+  messagingSenderId: "61462748636",
+  appId: "1:61462748636:web:625bc19140c5f119be0319",
+  measurementId: "G-RQESMXS1FY"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
